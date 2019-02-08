fb857e27b531a718a4dd42a01a1392e2aa2d8103
vivian <vivian@vivians-MacBook-Pro.local>
2019-02-08 12:21:36 -0700

    shiz kinda wonky with having to refresh on rerouting, but is working. maybe something wrong with withRouter now that connect is there

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import {Link, withRouter} from 'react-router-dom'
+import { connect } from 'react-redux'
 
 function Nav(props){
+    const {username, profile_pic} = props
     if(props.location.pathname === '/'){
         return <div></div>
     } else {
         return (
-            <div>
+            <div className='Nav'>
                 Nav
                 <Link to='/dashboard'><button>HOME</button></Link>
                 <Link to='/new'><button>NEW POST</button></Link>
                 <Link to='/'><button>LOGOUT</button></Link>
+                <img src={profile_pic} alt='profilepic'/>
+                <p>{username}</p>
             </div>
         )
     }
 }
 
-export default withRouter(Nav)
+const mapStateToProps = reduxState => {
+    return {
+        username: reduxState.username,
+        profile_pic: reduxState.profile_pic
+    }
+}
+
+export default connect(mapStateToProps)(withRouter(Nav))
